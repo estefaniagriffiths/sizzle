2f2827bef05a4a615275c13e091bc9ef17f5f08d
Lexline Johnson <lexlinejj27@gmail.com>
2025-04-25 14:26:59 -0400

    add comments

--- app/(tabs)/PostView.tsx
+++ app/(tabs)/PostView.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { Image as ExpoImage } from 'expo-image';
-import { Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
+import FontAwesome from 'react-native-vector-icons/FontAwesome6';
 
 export default function PostView({ post }: { post: any }) {
     const router = useRouter();
@@ -12,31 +12,31 @@ export default function PostView({ post }: { post: any }) {
     return (
         <View style={styles.card}>
             <TouchableOpacity onPress={() => router.push(`/post/${post.id}`)}>
-                <ExpoImage source={{ uri: post.image_link }} style={styles.image}
-                    contentFit="cover"
-                    transition={100}
-                />
-                <View style={[styles.align_left, {padding: 12}]}>
-                    <Text style={[styles.title, {paddingBottom: 6}]}>{post.title}</Text>
-                    <Text style={{ paddingBottom: 12 }}>
-                        <Text style={{ fontWeight: 'bold' }}>@{post.profiles?.username}</Text> {post.description}
-                    </Text>
-                    {post.post_tags && post.post_tags.length > 0 && (
-                        <View style={[styles.hstack, { paddingBottom: 12, flexWrap: 'wrap' }]}>
-                            {post.post_tags.map((pt: any, index: number) => (
-                                <View key={index} style={{ paddingRight: 6, paddingBottom: 6 }}>
-                                    <Text style={styles.tag}>{pt.tags.name}</Text>
-                                </View>
-                            ))}
-                        </View>
-                    )}
-                </View>
+            <ExpoImage source={{ uri: post.image_link }} style={styles.image}
+            contentFit="cover"
+            transition={100}
+            />
             </TouchableOpacity>
-
-            <View style={[styles.hstack, {justifyContent: 'space-between', padding: 12 }]}>
-                <Ionicons name="chatbubble-outline" size={24} color="black" />
-                <Ionicons name="bookmark-outline" size={24} color="black" />
-                <Ionicons name="heart-outline" size={24} color="black" />
+            <View style={[styles.align_left, {padding: 12}]}>
+                <Text style={[styles.user, {paddingBottom: 6}]}>@{post.profiles?.username}</Text>
+                <Text style={[styles.title, {paddingBottom: 6}]}>{post.title}</Text>
+                <Text style={{ paddingBottom: 12 }}>
+                    {post.description}
+                </Text>
+                {post.post_tags && post.post_tags.length > 0 && (
+                    <View style={[styles.hstack, { paddingBottom: 12, flexWrap: 'wrap' }]}>
+                        {post.post_tags.map((pt: any, index: number) => (
+                        <View key={index} style={{ paddingRight: 6, paddingBottom: 6 }}>
+                            <Text style={styles.tag}>{pt.tags.name}</Text>
+                        </View>
+                        ))}
+                    </View>
+                    )}
+                <View style={[styles.hstack, {paddingLeft: '60%', justifyContent: 'space-between'}]}>
+                    <FontAwesome name="message" size={18} color="black" />
+                    <FontAwesome name="heart" size={18} color="black" />
+                    <FontAwesome name="bookmark" size={18} color="black" />
+                </View>
             </View>
         </View>
     );
@@ -58,6 +58,9 @@ const styles = StyleSheet.create({
         backgroundColor: "white",
         borderRadius: 12,
         overflow: 'hidden'
+    },
+    user: {
+        color: 'darkorange'
     },
     title: {
         fontSize: 18,
--- app/post/[id].tsx
+++ app/post/[id].tsx
@@ -1,9 +1,10 @@
-import { useLocalSearchParams, useRouter } from 'expo-router';
-import { ScrollView, View, Text, StyleSheet, ActivityIndicator, TouchableOpacity,} from 'react-native';
-import { useEffect, useState } from 'react';
+import { useLocalSearchParams, useFocusEffect, useRouter } from 'expo-router';
+import { ScrollView, View, Text, StyleSheet, ActivityIndicator, TextInput, Alert, KeyboardAvoidingView, TouchableOpacity} from 'react-native';
+import { useEffect, useState, useCallback } from 'react';
 import { supabase } from '../../lib/supabase';
 import { Image as ExpoImage } from 'expo-image';
-import { Ionicons } from '@expo/vector-icons';
+import CommentView from '../CommentView';
+import FontAwesome from 'react-native-vector-icons/FontAwesome6';
 
 type PostType = {
   id: string;
@@ -27,9 +28,63 @@ export default function PostDetail() {
   const router = useRouter();
   const [post, setPost] = useState<PostType | null>(null);
   const [loading, setLoading] = useState(true);
+  const [comments, setComments] = useState<any[]>([]);
+  const [userId, setUserId] = useState('');
+  const [username, setUsername] = useState('');
+  const [newCommentContent, setNewCommentContent] = useState('');
+  const [posting, setPosting] = useState(false);
 
-  useEffect(() => {
-    if (!id) return;
+  const fetchComments = async () => {
+    if (post?.id == null) {
+      console.log("id is null: ", post?.id)
+      return;
+    }
+    const { data, error } = await supabase
+    .from('comments')
+    .select('created_at, user_id, content')
+    .eq('post_id', post?.id)
+    .order('created_at', { ascending: false });
+
+    console.log('Fetched comments:', data);
+
+    if (error) {
+    console.error('Error fetching comments:', error);
+    } else {
+      setComments(data || []);
+    }
+    setLoading(false);
+  };
+
+  async function getUser() {
+    const { data: { user } } = await supabase.auth.getUser();
+    if (user) {
+      const { data, error } = await supabase
+        .from('profiles')
+        .select('username, bio')
+        .eq('id', user.id)
+        .single();
+
+      if (error) {
+        console.error('Error retrieving profile:', error.message);
+      } else {
+        setUsername(data.username);
+      }
+
+      setLoading(false);
+    }
+  }
+
+  useFocusEffect(
+  useCallback(() => {
+      supabase.auth.getSession().then(({ data: { session } }) => {
+        // setSession(session);//
+        setUserId(session?.user?.id || "");
+      })
+  
+      supabase.auth.onAuthStateChange((_event, session) => {
+        // setSession(session);
+        setUserId(session?.user?.id || "");
+      });      
 
     const fetchPost = async () => {
       const { data, error } = await supabase
@@ -46,8 +101,66 @@ export default function PostDetail() {
       setLoading(false);
     };
 
+    getUser();
+
+    console.log("fetching posts...")
     fetchPost();
-  }, [id]);
+    console.log("fetching comments...")
+    fetchComments();
+  }
+  ,
+  [id]));
+
+  
+
+  const handlePostComment = async () => {
+      // if (!newCommentContent) {
+      //   Alert.alert("Missing Fields", "Please fill out all fields and select an image.");
+      //   return;
+      // }
+      console.log("comment: ", newCommentContent)
+      console.log("post: ", newCommentContent)
+      console.log("user id: ", userId)
+      console.log("post id: ", post?.id)
+      if (post?.id == null) {
+        return;
+      }
+    
+      try {
+        setPosting(true);
+    
+        const { data: insertedComments, error: insertError } = await supabase
+        .from('comments')
+        .insert([
+          {
+            created_at: new Date().toISOString(),
+            user_id: userId,
+            content: newCommentContent,
+            post_id: post?.id
+          },
+        ])
+        .select();
+    
+        if (insertError) {
+          console.error(insertError);
+          Alert.alert("Post Error", "Failed to save comment data.");
+          return;
+        }
+  
+        const commentId = insertedComments[0].id;
+    
+        Alert.alert("Success", "Comment posted successfully!");
+    
+      } catch (err) {
+        console.error(err);
+        Alert.alert("Unexpected Error", "Something went wrong.");
+      } finally {
+        setPosting(false);
+        getUser();
+        console.log("user: ", username);
+        fetchComments();
+      }
+  };
 
   if (loading) return <ActivityIndicator size="large" style={{ marginTop: 60 }} />;
   if (!post) return <Text>Post not found</Text>;
@@ -56,31 +169,21 @@ export default function PostDetail() {
   const recipeSteps = post.recipe?.split('\n').filter(s => s.trim() !== '');
 
   return (
-    <ScrollView style={{ flex: 1, backgroundColor: 'white' }}>
-      <View style={styles.header}>
-        <TouchableOpacity onPress={() => router.back()}>
-          <Ionicons name="arrow-back" size={28} color="black" />
-        </TouchableOpacity>
-        <Text style={styles.headerTitle}>Post Details</Text>
-        <View style={{ width: 28 }} />
-      </View>
-
-      <View style={{ padding: 20 }}>
-        <ExpoImage source={{ uri: post.image_link }} style={styles.image} contentFit="cover" transition={100} />
+    <View style={{ padding: 20, marginTop: 50, marginBottom: 64 }}>
+      <ScrollView>
+        <ExpoImage source={{ uri: post.image_link }} style={styles.image} contentFit="cover" />
+        <Text style={styles.user}>@{post.profiles?.username}</Text>
         <Text style={styles.title}>{post.title}</Text>
-        <Text style={styles.username}>@{post.profiles?.username}</Text>
         <Text style={styles.description}>{post.description}</Text>
-
         {post.post_tags && post.post_tags.length > 0 && (
-          <View style={[styles.hstack, { paddingBottom: 12, flexWrap: 'wrap' }]}>
+          <View style={[styles.hstack, { flexWrap: 'wrap' }]}>
             {post.post_tags.map((pt, index) => (
               <View key={index} style={{ paddingRight: 6, paddingBottom: 6 }}>
                 <Text style={styles.tag}>{pt.tags.name}</Text>
               </View>
             ))}
-          </View>
+            </View>
         )}
-
         <Text style={styles.sectionTitle}>Ingredients</Text>
         {ingredientsList?.map((item, index) => (
           <Text key={index} style={{ marginBottom: 5, fontSize: 15 }}>{'\u2022'} {item.trim()}</Text>
@@ -90,8 +193,34 @@ export default function PostDetail() {
         {recipeSteps?.map((step, index) => (
           <Text key={index} style={{ marginBottom: 12, fontSize: 15 }}>{index + 1}. {step.trim()}</Text>
         ))}
-      </View>
-    </ScrollView>
+        {loading ? (
+            <ActivityIndicator size="large" color="#BB3E03" />
+        ) : (
+            comments.map((comment) => (
+                <CommentView comment={comment} username={username} />
+            ))
+        )}
+      </ScrollView>
+      <KeyboardAvoidingView
+          style={{flex: 1}}
+          keyboardVerticalOffset={100}
+          behavior={"position"}
+        >
+        <View style={[styles.hstack, {width: '100%', height: 60, backgroundColor: "#F2F2F7FF"}]}>
+        <TextInput
+            style={styles.input}
+            placeholder="Reply"
+            placeholderTextColor={"lightgray"}
+            maxLength={500}
+            value={newCommentContent}
+            onChangeText={setNewCommentContent}
+        />
+        <TouchableOpacity onPress={handlePostComment} disabled={posting}>
+          <FontAwesome style={{paddingLeft: 10, paddingTop: 20}} name="paper-plane" size={18} color="black" />
+        </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    </View>
   );
 }
 
@@ -121,19 +250,19 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     marginBottom: 16,
   },
+  user: {
+    fontSize: 16,
+    color: 'darkorange',
+    marginBottom: 12
+  },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 4,
   },
-  username: {
-    fontSize: 16,
-    marginBottom: 12,
-    color: 'gray',
-  },
   description: {
     fontSize: 16,
-    marginBottom: 20,
+    marginBottom: 12,
   },
   sectionTitle: {
     fontSize: 18,
@@ -152,6 +281,15 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignContent: 'center',
     flexWrap: 'wrap',
+  },
+  input: {
+    width: '90%',
+    backgroundColor: 'white',
+    color: 'black',
+    borderRadius: 10,
+    padding: 15,
+    marginVertical: 10,
+    fontSize: 16,
   },
 });
 
